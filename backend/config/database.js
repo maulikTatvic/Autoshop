96e94ad0786a1d8f2abc4b713c8b59092c5e46a4
maulikTatvic <maulik.patel@tatvic.com>
2023-05-12 14:19:45 +0530

    add json file into backend

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,7 @@
 
 const mongoose = require('mongoose');
-
+const dotenv = require("dotenv");  //require dotenv package
+dotenv.config({ path: ".config/config.env" }); 
 
 const connectDatabase = () => {
     mongoose.connect(process.env.DB_LOCAL_URI, {
